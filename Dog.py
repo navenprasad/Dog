4ae915ed13777ce27f0702ebc53be7718b2d88b5
Naven Prasad <naven@leern.tech>
2017-12-01 20:37:07 +0800

    I added name attr

--- Dog.py
+++ Dog.py
@@ -2,12 +2,19 @@ class Dog:
     def __init__(self):
         self.pos_y = 0
         self.pos_x = 0
+        self.name = ""
 
     def moveUp(self):
         self.pos_y = self.pos_y + 1
 
+    def setName(self, this_name):
+        self.name = this_name
+
     def getPos(self):
         print str(self.pos_y)
         print str(self.pos_x)
+        print self.name
 
-doggy = Dog()   
+doggy = Dog()
+doggy.setName("Scooby")
+doggy.getPos()
